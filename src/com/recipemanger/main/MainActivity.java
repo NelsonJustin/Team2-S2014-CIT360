cd7068f02c62286134f32977c52775dfa5a33eb4
Justin <bansheeboy89@hotmail.com>
2014-06-14 14:05:18 -0600

    Justin Performance Review #3

--- src/com/recipemanger/main/MainActivity.java
+++ src/com/recipemanger/main/MainActivity.java
@@ -1,19 +1,21 @@
-package com.recipemanger.main;
+package com.recipemanager;
 
-
-import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
 
-public class MainActivity extends Activity
-{
-    /** Called when the activity is first created. */
-    
-    
-    
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+public class MainActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.registration_screen);
+	}
 }
