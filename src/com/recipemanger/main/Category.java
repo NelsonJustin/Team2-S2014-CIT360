29cbb824c991c1089bed71105a57554d977615cd
Justin <bansheeboy89@hotmail.com>
2014-07-08 19:29:57 -0600

    Justin Changes

--- src/com/recipemanger/main/Category.java
+++ src/com/recipemanger/main/Category.java
@@ -1,11 +1,11 @@
 package com.example.myrecipemanager;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import android.support.v7.*;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.*;
 import android.view.*;
+import android.widget.Button;
 
 
 public class Category extends Activity {
@@ -15,47 +15,31 @@ public class Category extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.category_screen);
 
-		/*if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}*/
+		categoryAdd();
+		
 	}
 
-	/*@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.category, menu);
-		return true;
+	public void categoryAdd() {
+		//1. Get reference to button
+		Button addCategory = (Button) findViewById(R.id.add_button);
+		
+		//2. Set Onclick Listener for addButton
+		addCategory.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				try{
+				LayoutInflater c = getLayoutInflater();
+				View parentLayout = c.inflate(R.layout.add_category,null);
+				AlertDialog.Builder categoryDialog = new AlertDialog.Builder(Category.this);
+				categoryDialog.setView(parentLayout).show();
+				} catch(InflateException e){
+					e.printStackTrace();
+				}
+				
+			}
+		});
 	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	/*public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_category,
-					container, false);
-			return rootView;
-		}
-	}*/
 
 }
