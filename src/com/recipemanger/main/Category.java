ce4fd722c4b756dc53710b8ead1c1e44e6fb9bb5
Justin <bansheeboy89@hotmail.com>
2014-07-13 23:57:23 -0600

    More Justin Changes

--- src/com/recipemanger/main/Category.java
+++ src/com/recipemanger/main/Category.java
@@ -7,16 +7,15 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.os.*;
 import android.view.*;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.ListView;
+import android.widget.*;
 
 
 
 public class Category extends Activity {
 
 	private ArrayList<CategoryBean> categories = new ArrayList<CategoryBean>();
-
+	private CategoryBean category = new CategoryBean();
+	private int bean = 0;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -44,14 +43,54 @@ public class Category extends Activity {
 			break;
 		
 	}
+		
+}
 	
-} 
+	public void saveCategory(View i){
+		 
+		createCategory();
+		
+		//Inflate Category Screen with new Category Text View
+		i.getId();
+		LayoutInflater s = getLayoutInflater();
+		s.inflate(R.layout.category_screen, null);
+
+		}
 	
-	public void newCategory(View i){
+	private ArrayList<CategoryBean> newCategory(){
+			
 		//Reference New Category Input
-		EditText item = (EditText) i.findViewById(R.id.new_category);
+		EditText item = (EditText) findViewById(R.id.new_category);
 		
+		//Use the user input to create CategoryBean Object and add the object to a collection.
+		while(!item.equals(null)){
+			
+			category.setCategoryName(item.toString());
+			categories.add(category);
+			
+		}
 		
+		return categories;
 		
-	} 
+	}
+	
+	private void createCategory(){
+		
+		int c = 1;
+		LinearLayout categoryRow = (LinearLayout) findViewById(R.id.category_list);
+		TextView categoryNew = (TextView) findViewById(R.layout.category_rows);
+		
+		//Get User input and create CategoryBean
+		newCategory();
+		
+		//Set TextView Id, add TextView to LinearLayout to create Category List, Increment ID for the next additional category
+		categoryNew.setId(c);
+	    categoryRow.addView(categoryNew, R.layout.category_rows);
+	    
+	    c++;
+	    
+	    //Set TextView text using the CategoryBean ArrayList
+	    categoryNew.setText(categories.get(bean).getCategoryName());
+	    
+	}
 }
